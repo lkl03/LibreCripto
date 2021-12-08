85714307e691b28f3bdf18a2bbc7c6d90710feff
lkl03 <95750269+lkl03@users.noreply.github.com>
2021-12-08 15:51:53 -0300

    Add files via upload
    Actual Version of LibreCripto (V1.1) at 08/12/21

---
+++ includes/templates/footerop.php
@@ -0,0 +1,62 @@
+<footer class="site-footer">
+            <div class="contenedor clearfix">
+                <div class="footer-menu">
+                    <ul>
+                        <!--<li><a href="#">Nuestro blog</a></li>-->
+                        <li><a href="https://librecripto.com/nosotros">Sobre LibreCripto</a></li>
+                        <li><a href="https://librecripto.com/help">Ayuda</a></li>
+                        <li><a href="https://librecripto.com/contacto">Contactanos</a></li>
+                        <li><a href="https://librecripto.com/terminos-y-condiciones">Términos y condiciones</a></li>
+                        <li><a href="https://librecripto.com/politica-de-privacidad.php">Privacidad</a></li>
+                    </ul>
+                </div>
+                <div class="social">
+                    <div class="logoFooter">
+                        <img src="img/logo.svg" alt="logo librecripto">
+                    </div>
+                </div>
+<div class="redes">
+                    <nav>
+                        <a href="https://www.facebook.com/libre_cripto" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/libre_cripto" target="_blank"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/libre_cripto/" target="_blank"><i class="fab fa-instagram"></i></a>
+                        <a href="https://t.me/+iZwZ1GE4J2sxZDUx" target="_blank"><i class="fab fa-telegram"></i></a>
+                    </nav>
+                    <p class="lcversion">V1.1</p>
+                </div>
+            </div>
+            <p class="disclaimer"> LIBRECRIPTO no es una entidad financiera ni ofrece servicios de ningún tipo más que la posibilidad de conectar en tiempo y forma a diferentes clientes afines a la modalidad de comercio P2P y F2F.</p>
+            <p class="copyright">LIBRECRIPTO. © Todos los derechos reservados 2021.</p>
+
+
+
+        </footer>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/operations.js"></script>
+    <script src="js/usersidebar.js"></script>
+
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+        window.ga = function() {
+            ga.q.push(arguments)
+        };
+        ga.q = [];
+        ga.l = +new Date;
+        ga('create', 'G-J8JNT9JNLG', 'auto');
+        ga('set', 'anonymizeIp', true);
+        ga('set', 'transport', 'beacon');
+        ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
+
+</body>
+
+</html>
---
+++ includes/templates/headeraccess.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html class="no-js" lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $title ?></title>
+    <meta name="description" content="contacto de usuarios y comercio de criptomonedas">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="LibreCripto">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://librecripto.com">
+    <meta property="og:image" content="https://librecripto.com/img/content.png">
+    <meta property="og:description" content="Comerciá tus criptomonedas como vos prefieras. Bienvenido a LibreCripto, bienvenido al mercado P2P y F2F." />
+    <meta property="og:locale" content="es_ES" />
+
+    <link rel="icon" href="favicon.ico">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400;700&family=Nunito:wght@200;300;400;600&family=Raleway:wght@200;300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
+
+    <meta name="theme-color" content="#fe9416">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script type="text/javascript">
+    $(window).load(function() {
+        $(".loader").fadeOut("slow");
+    });
+    </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J8JNT9JNLG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J8JNT9JNLG');
+</script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2486691799966384"
+     crossorigin="anonymous"></script>
+</head>
+
+<body>
+<div class="loader"></div>
+
+    <header class="site-header">
+        <div class="contenedor clearfix">
+            <div class="logoAcceso">
+                <a href="https://librecripto.com/">
+                    <img src="img/logo.svg" alt="logo librecripto">
+                </a>
+            </div>
+        </div>
+    </header>
