5c6623cc087c7393326e9536ea92bc6d271156c8
lkl03 <95750269+lkl03@users.noreply.github.com>
2021-12-08 15:40:51 -0300

    Add files via upload

---
+++ php/users.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    include 'conexion_be.php';
+    $outgoing_id = $_SESSION['user'];
+    $sql = "SELECT * FROM usuarios WHERE user = {$outgoing_id}  ORDER BY user DESC";
+    $query = mysqli_query($conexion, $sql);
+    $output = "";
+    if(mysqli_num_rows($query) == 0){
+        $output .= "No users are available to chat";
+    }elseif(mysqli_num_rows($query) > 0){
+        include 'data.php';
+    }
+    echo $out;
+?>
